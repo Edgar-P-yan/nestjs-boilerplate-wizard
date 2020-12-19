f5207440a34dc1f1bf81782f4e477e823ab89478
Edgar <starguitar307@gmail.com>
2020-12-19 02:17:36 +0200

    refactor: format some code

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,21 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
-const logger = new Logger('Bootstrap')
+const logger = new Logger('Bootstrap');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService)
-  const appPort = configService.get<number>('app.port')
-  const appHost = configService.get<string>('app.host')
+  const configService = app.get(ConfigService);
+  const appPort = configService.get<number>('app.port');
+  const appHost = configService.get<string>('app.host');
 
   await app.listen(appPort, appHost, () => {
-    logger.log(`The server is listening on http://${appHost}:${appPort}`)
+    logger.log(`The server is listening on http://${appHost}:${appPort}`);
   });
 }
 
-bootstrap().catch(err => {
-  logger.error(err)
-  process.exit(1)
+bootstrap().catch((err) => {
+  logger.error(err);
+  process.exit(1);
 });

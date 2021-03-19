b0c981a510823aa7989a00d914444401c5e8dd4f
Edgar <starguitar307@gmail.com>
2021-03-19 02:06:24 +0200

    feat: improve app.config.ts

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,10 @@
-import { registerAs } from '@nestjs/config';
+import { ConfigType, registerAs } from '@nestjs/config';
 
 export const appConfigsFactory = registerAs('app', () => ({
   port: parseInt(process.env.PORT || '3000'),
   host: process.env.HOST || 'localhost',
 }));
+
+export const APP_CONFIGS_KEY = appConfigsFactory.KEY;
+
+export type TAppConfigs = ConfigType<typeof appConfigsFactory>;
